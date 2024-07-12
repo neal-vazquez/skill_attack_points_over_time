d6eee9759f7d973c1bdf80d17181b603bde66912
neal-vazquez <neal.vazquez@ischool.berkeley.edu>
2024-07-12 00:14:23 -0700

    13th push

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Log the installation process
-exec > >(tee -i /tmp/setup.log)
-exec 2>&1
+# Install Node.js and npm
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y nodejs
 
 # Install Playwright browsers with dependencies
 npx playwright install --with-deps
