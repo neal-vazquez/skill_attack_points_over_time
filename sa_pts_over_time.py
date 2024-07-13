dcb21923821ed639e5cec06082e4b93d51e1e60c
neal-vazquez <neal.vazquez@ischool.berkeley.edu>
2024-07-13 00:31:30 -0700

    yes 8

--- sa_pts_over_time.py
+++ sa_pts_over_time.py
@@ -83,7 +83,7 @@ def plot_data(data, username, user_code):
     for year, gain in yearly_gain.items():
         max_date = df[df['Year'] == year]['Date'].max()
         max_skill = df[df['Year'] == year]['Skill Point'].max()
-        plt.text(max_date, max_skill, f'+{gain:.2f}', fontsize=9, ha='right', va='bottom')
+        plt.text(max_date, max_skill, f'+{abs(gain):.2f}', fontsize=9, ha='right', va='bottom')
 
     plt.title(f'Skill Points Over Time with Yearly Gains\nFor Player: {username} ({formatted_user_code})')
     plt.xlabel('Date')
